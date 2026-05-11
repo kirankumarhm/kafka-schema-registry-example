3d9d12056fb78483c6c680e1062b8f29c1ff3a76
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-05-11 19:49:14 +0530

    Create KafkaSchemaRegistryExampleApplication class

---
+++ src/main/java/com/kafka/schema/registry/example/KafkaSchemaRegistryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.schema.registry.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSchemaRegistryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSchemaRegistryExampleApplication.class, args);
+	}
+
+}
